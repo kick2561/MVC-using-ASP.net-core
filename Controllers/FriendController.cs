bab7fc226bfbc76a9d38f876a55bb9cd7f12b3cb
Bonu <cb185249@ncr.com>
2020-03-23 15:56:28 +0530

    CRUD operations using asp.net core

---
+++ Controllers/FriendController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Task.Controllers
+{
+    [Route("friend")]
+    public class FriendController : Controller
+    {
+        private readonly FriendDataContext Data;
+
+        public FriendController(FriendDataContext db)
+        {
+            Data = db;
+        }
+        // GET: /<controller>/
+        [Route("")]
+        public IActionResult Index()
+        {
+            var friends = Data.Friends.ToArray();
+            return View(friends);
+        }
+
+        [HttpGet, Route("update")]
+        public IActionResult Update()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("update")]
+        public IActionResult Update(IFormCollection form)
+        {
+            var id = int.Parse(Request.Form["FriendId"]);
+            var result = Data.Friends.SingleOrDefault(x => x.FriendId == id);
+            if (result != null)
+            {
+                result.FriendName = Request.Form["FriendName"];
+                result.Place = Request.Form["Place"];
+                Data.SaveChanges();
+            }
+
+            return View();
+        }
+
+        [HttpGet, Route("create")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("create")]
+        public IActionResult Create(Friend friend)
+        {
+            if (!ModelState.IsValid)
+                return View();
+            Data.Friends.Add(friend);
+            Data.SaveChanges();
+            return View();
+        }
+
+        [HttpGet , Route("Delete")]
+
+        public IActionResult Delete()
+        {
+            return View();
+        }
+
+        [HttpPost, Route("Delete")]
+
+        public IActionResult Delete(Friend friend)
+        {
+            var id = int.Parse(Request.Form["FriendId"]);
+            var result = Data.Friends.SingleOrDefault(x => x.FriendId == id);
+            if (result != null)
+            {
+                Data.Friends.Remove(result);
+                Data.SaveChanges();
+            }
+
+            return View();
+        }
+    }
+    
+    
+}
---
+++ Models/Friend.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task.Models
+{
+    public class Friend
+    {
+        public long Id { get; set; }
+        [Required]
+        public int FriendId { get; set; }
+
+        [Required]
+        public string FriendName { get; set; }
+        [MaxLength(25, ErrorMessage =
+            "Friend's place must not be greater than 25 characters")]
+
+        public string Place { get; set; }
+
+    }
+}
